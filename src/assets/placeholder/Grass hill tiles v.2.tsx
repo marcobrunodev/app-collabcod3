3bfc7c31593aa87aeb7d902592274106c2a21d21
Marco Bruno <marco.bruno@meliuz.com.br>
2023-01-02 21:03:56 +0100

    :fire: Remove files into assets folder

--- src/assets/placeholder/Grass hill tiles v.2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Grass hill tiles v.2" tilewidth="16" tileheight="16" tilecount="77" columns="11">
- <image source="../plaholder-active/Grass hill tiles v.2.png" width="176" height="112"/>
-</tileset>
--- src/assets/placeholder/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="water" tilewidth="16" tileheight="16" tilecount="4" columns="4">
- <image source="../plaholder-active/water.png" width="64" height="16"/>
-</tileset>
